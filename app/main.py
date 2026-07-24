1e565e8d1ea8c18f27772e0c1aed2542f0279550
iiismailtriki <ismail.triki@esprit.tn>
2026-07-24 20:40:29 +0200

    security: make application bind address configurable

--- app/main.py
+++ app/main.py
@@ -55,5 +55,6 @@ app = create_app()
 
 
 if __name__ == "__main__":
+    host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host=host, port=port)
